f7b8c9cbbb5f56540feb130f1230c4d55628db90
Sumeet Yadav <sumeetishwar1610@gmail.com>
2023-10-05 18:14:29 +0530

    13_3D_Text_Final_Code

--- 13_3D_Text/src/script.js
+++ 13_3D_Text/src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 
 THREE.ColorManagement.enabled = false
 
@@ -16,10 +18,81 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Axes Helper 
+const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png');
+
+/**
+ * Fonts
+ */
+const fontLoader = new FontLoader()
+
+fontLoader.load(
+    'fonts/helvetiker_regular.typeface.json',
+    (font) => {
+        const textGeometry = new TextGeometry(
+            'Hello Three.js',
+            {
+                font: font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 5,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 3
+            }
+        )
+
+        // Hard Way To Center
+        // textGeometry.computeBoundingBox()
+        // console.log(textGeometry.boundingBox);
+
+        // textGeometry.translate(
+        //     - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
+        //     - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
+        //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5,
+        // )
+
+        // Easy Way to Center
+        textGeometry.center();
+
+        const textMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+        const text = new THREE.Mesh(textGeometry, textMaterial)
+        // textMaterial.wireframe = true;
+        scene.add(text)
+
+        console.time("donut");
+
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+
+        for (let i = 0; i < 100; i++) {
+            const donut = new THREE.Mesh(donutGeometry, textMaterial);
+
+            donut.position.x = (Math.random() - 0.5) * 10;
+            donut.position.y = (Math.random() - 0.5) * 10;
+            donut.position.z = (Math.random() - 0.5) * 10;
+
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+
+            scene.add(donut)
+
+        }
+
+        console.timeEnd('donut');
+    }
+)
 
 /**
  * Object
@@ -29,7 +102,7 @@ const cube = new THREE.Mesh(
     new THREE.MeshBasicMaterial()
 )
 
-scene.add(cube)
+// scene.add(cube)
 
 /**
  * Sizes
@@ -39,8 +112,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -83,8 +155,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update controls
