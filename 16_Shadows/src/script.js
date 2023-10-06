43fbb22829df3d1b1c3bc82e86df5dcab2ff55e3
Sumeet Yadav <sumeetishwar1610@gmail.com>
2023-10-07 01:00:27 +0530

    16_Shadows_Till_PCFSoftShadowMap

--- 16_Shadows/src/script.js
+++ 16_Shadows/src/script.js
@@ -33,6 +33,23 @@ gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(directionalLight)
 
+directionalLight.castShadow = true;
+
+directionalLight.shadow.mapSize.width = 1024;
+directionalLight.shadow.mapSize.height = 1024;
+directionalLight.shadow.camera.top = 2;
+directionalLight.shadow.camera.right = 2;
+directionalLight.shadow.camera.bottom = -2;
+directionalLight.shadow.camera.left = -2;
+directionalLight.shadow.camera.near = 1;
+directionalLight.shadow.camera.far = 6;
+directionalLight.shadow.radius = 10;
+
+// Camera Helper for Near and Far
+
+const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+scene.add(directionalLightCameraHelper);
+
 /**
  * Materials
  */
@@ -49,12 +66,16 @@ const sphere = new THREE.Mesh(
     material
 )
 
+sphere.castShadow = true;
+
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
     material
 )
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
+
+plane.receiveShadow = true; 
 
 scene.add(sphere, plane)
 
@@ -104,6 +125,9 @@ const renderer = new THREE.WebGLRenderer({
 renderer.outputColorSpace = THREE.LinearSRGBColorSpace
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 /**
  * Animate
