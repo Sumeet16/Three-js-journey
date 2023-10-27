a5b4d0a48f6f387bbe05b307636b2a5981be7696
Sumeet Yadav <sumeetishwar1610@gmail.com>
2023-10-28 00:25:06 +0530

    22_Imported_Models_Starter_Code

--- 21_Physics/src/script.js
+++ 21_Physics/src/script.js
@@ -93,7 +93,6 @@ const world = new CANNON.World();
 world.broadphase = new CANNON.SAPBroadphase(world);
 world.allowSleep = true;
 world.gravity.set(0, -9.82, 0);
-
 // Material 
 const defaultMaterial = new CANNON.Material("default");
 
